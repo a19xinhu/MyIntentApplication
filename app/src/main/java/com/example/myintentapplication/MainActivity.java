0461b638960ea1a18c34b9b17b25ba6418832977
a19xinhu <tmxk93@hotmail.com>
2022-04-13 13:45:26 +0200

    Add Second Activity, button in MainActivity

---
+++ app/src/main/java/com/example/myintentapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myintentapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonClick();
+            }
+        });
+    }
+
+    private void onButtonClick(){
+        Log.d("MainActivity","I clicked the button.");
+        Toast.makeText(this,"The button I clicked",Toast.LENGTH_LONG).show();
+    }
+}
